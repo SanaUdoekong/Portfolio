0163d6f6d8ca1c29c7b4c449805ae0580a024532
Sana Udoekong <udoekongsana@gmail.com>
2021-05-05 23:44:33 +0100

    Final Design

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
+final Color darkBlue = Color.fromARGB(255, 18, 32, 47);
+final _colors = [
+  Colors.blueGrey,
+  Colors.grey,
+  Colors.blueGrey
+];
+final _stops = [
+  0.0,
+  0.7,
+  0.0
+];
+
 void main() {
   runApp(MyApp());
 }
@@ -7,186 +19,123 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: MyProfile(), debugShowCheckedModeBanner: false);
-  }
-}
+    return MaterialApp(
+      // theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: darkBlue),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Portfolio',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ), // Text Styles
+          ), // Title
+          backgroundColor: Colors.blueGrey,
+        ), // App Bar
 
-class MyProfile extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final screenHeight = MediaQuery.of(context).size.height;
-    final screenWidth = MediaQuery.of(context).size.width;
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.lightBlue[50],
         body: Stack(
           children: [
-            Column(
-              children: [
-                Expanded(
-                  flex: 2,
-                  child: ClipPath(
-                    clipper: ContainerClipper(),
+            Expanded(
+              child: Column(
+                children: [
+                  Expanded(
                     child: Container(
-                      decoration: BoxDecoration(color: Colors.blue[900]),
+                      padding: EdgeInsets.only(bottom: 45, left: 30, right: 30),
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          colors: _colors,
+                          stops: _stops,
+                        ), // Gradient
+                      ), // Decoration
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Column(
+                              children: [
+                                Container(
+                                  height: 85,
+                                  width: 85,
+                                  margin: EdgeInsets.only(top: 30, bottom: 10),
+                                  child: CircleAvatar(
+                                    radius: 50,
+                                    backgroundImage: AssetImage('assets/images/me.jpg'),
+                                  ),
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: Colors.grey,
+                                  ), // Decoration
+                                ), // Display Image
+                                Container(
+                                  alignment: Alignment.center,
+                                  margin: EdgeInsets.only(bottom: 5),
+                                  child: Text(
+                                    'Sana Idemakpan Udoekong',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 20,
+                                    ), // Text Style
+                                  ), // Text
+                                ), // Name
+
+                                Container(
+                                  alignment: Alignment.center,
+                                  margin: EdgeInsets.only(bottom: 40),
+                                  child: Text(
+                                    'Student | Aspiring Software Engineer | Music Freak | Technophile | Innovator',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 15,
+                                    ), // Text Styles
+                                  ), // Description
+                                ), // Name
+
+                                Column(
+                                  children: [
+                                    _buildButtonColumn(Colors.blueGrey, Icons.call, '+2347089404857'),
+                                    _buildButtonColumn(Colors.blueGrey, Icons.email, 'udoekongsana@gmail.com'),
+                                    _buildButtonColumn(Colors.blueGrey, Icons.home, 'Ikpe Udok'),
+                                  ],
+                                ), // Other details
+                              ], // Name and Description
+                            ), // Columns for name and description
+                          ), // Expanded
+                        ], // Columns and rows
+                      ), // Home
                     ),
                   ),
-                ),
-                Expanded(
-                  flex: 6,
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Text('Sana Udoekong',
-                          style: TextStyle(
-                            color: Colors.blue[900],
-                            fontSize: 31,
-                            fontWeight: FontWeight.bold,
-                          )),
-                      Text(
-                        'FLUTTER DEVELOPER',
-                        style: TextStyle(
-                            color: Colors.blue[900],
-                            fontSize: 19,
-                            letterSpacing: 2.0),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(horizontal: 12, vertical: 4),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 8),
-                          child: Text(
-                              'I\'m kin on using my skills to profer positive solutions to world issues',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                  color: Colors.blue[900],
-                                  fontSize: 15,
-                                  letterSpacing: 2.0)),
-                        ),
-                      ),
-                      SizedBox(
-                        width: 150,
-                        child: Divider(
-                          color: Colors.blue[900],
-                        ),
-                      ),
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(horizontal: 12, vertical: 4),
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 8),
-                          child: Text(
-                              'I\'m Student | Aspiring software developer | Music freak | Technophile ',
-                              textAlign: TextAlign.center,
-                              style: TextStyle(
-                                  color: Colors.blue[900],
-                                  fontSize: 15,
-                                  letterSpacing: 2.0)),
-                        ),
-                      ),
-                      SizedBox(
-                        height: 20,
-                        width: 150,
-                        child: Divider(
-                          color: Colors.blue[900],
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16),
-                        child: Card(
-                          color: Colors.blue[900],
-                          margin:
-                              EdgeInsets.symmetric(horizontal: 12, vertical: 5),
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                                vertical: 8, horizontal: 10),
-                            child: ListTile(
-                                leading: Icon(
-                                  Icons.call,
-                                  color: Colors.lightBlue[50],
-                                ),
-                                title: Text('+2347089404857',
-                                    style: TextStyle(
-                                        color: Colors.lightBlue[50]))),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 16),
-                        child: Card(
-                          color: Colors.blue[900],
-                          margin:
-                              EdgeInsets.symmetric(horizontal: 12, vertical: 5),
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                                vertical: 8, horizontal: 10),
-                            child: ListTile(
-                                leading: Icon(
-                                  Icons.email,
-                                  color: Colors.lightBlue[50],
-                                ),
-                                title: Text('udoekongsana@gmail.com',
-                                    style: TextStyle(
-                                        color: Colors.lightBlue[50]))),
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            Positioned(
-              // top: screenHeight * (1 / 8) - 100 / 2,
-              left: screenWidth * 0.28,
-              bottom: screenHeight - 220,
-              // right: 30,
-              child: Container(
-                margin: const EdgeInsets.all(4),
-                height: 150,
-                width: 150,
-                decoration: BoxDecoration(
-                    shape: BoxShape.circle, color: Colors.lightBlue[50]),
-                child: Container(
-                  padding: EdgeInsets.all(4),
-                  margin: const EdgeInsets.all(8),
-                  child: CircleAvatar(
-                    radius: 50,
-                    backgroundImage: AssetImage('assets/images/portfolio.jpg'),
-                  ),
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    border: Border.all(color: Colors.blue[900], width: 3),
-                  ),
-                ),
+                ],
               ),
-            )
+            ),
           ],
-        ),
-      ),
+        ), // Body
+      ), // Home
     );
   }
 }
 
-class ContainerClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    Path path = Path();
-
-    path.moveTo(0, size.height - 40);
-    path.quadraticBezierTo(
-        size.width / 2, size.height, size.width, size.height - 40);
-    path.lineTo(size.width, 0);
-    path.lineTo(0, 0);
-
-    return path;
-  }
-
-  @override
-  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => true;
+Column _buildButtonColumn(Color color, IconData icon, String label) {
+  return Column(
+    mainAxisSize: MainAxisSize.min,
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Icon(icon, color: color),
+      Container(
+        margin: const EdgeInsets.only(top: 8, bottom: 8),
+        child: Text(
+          label,
+          style: TextStyle(
+            fontSize: 12,
+            fontWeight: FontWeight.w400,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    ],
+  );
 }
+
+                       
+                       
+                            
+          
